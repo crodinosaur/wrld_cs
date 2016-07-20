1b80513ba952bd4ade99f26d6f5350e91a84d093
Dino Bilankov <dino.bilankov@gmail.com>
2016-07-20 19:06:14 -0400

    finished coming soon page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require unify/vendor/jquery-migrate.min
+//= require unify/vendor/bootstrap.min
+//= require unify/vendor/back-to-top
+//= require unify/vendor/smoothScroll
+//= require unify/vendor/jquery.plugin
+//= require unify/vendor/jquery.countdown
+//= require unify/vendor/jquery.backstretch.min
+//= require unify/custom
 //= require_tree .
